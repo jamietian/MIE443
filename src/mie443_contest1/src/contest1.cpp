b1511432a4037e39803bd7a804836e90eefc7bc7
Jamie Tian <42948354+jamietian@users.noreply.github.com>
2020-02-07 15:02:13 -0500

    Update contest1.cpp

--- src/mie443_contest1/src/contest1.cpp
+++ src/mie443_contest1/src/contest1.cpp
@@ -1,3 +1,7 @@
+/*
+MIE443 Mechatronics Design
+Team 18
+*/
 #define N_BUMPER (3)
 #define RAD2DEG(rad) ((rad) * 180. / M_PI)
 #define DEG2RAD(deg) ((deg) * M_PI / 180.)
@@ -24,16 +28,16 @@ float posX = 0.0, posY = 0.0, yaw = 0.0;
 geometry_msgs::Twist vel;
 
 
-float minLaserDist[12] = {};
+float minLaserDist[11] = {};
 float avgLaserDist[3] = {};
 int32_t nLasers=0, desiredNLasers=0, desiredAngle=25, edgeRange= 100;
 
-int32_t nLasers=0, desiredNLasers=0, desiredAngle=10, edgeRange= 100;
-
 float left_d, right_d, center_d;
+float avg_left, avg_right, avg_center;
 float reg_speed = 0.25;
-float turn_speed = 0.1;
-float ang_speed = 0.3;
+float turn_speed = 1;
+float ang_speed = 1;
+float scanAng_speed = 0.5;
 
 int corner_counter = 0;
 bool turned = false; 
@@ -42,6 +46,8 @@ bool turned = false;
 float wf_dis = 0.9;
 float f_limit = 0.9; 
 float l_limit = 1.5; 
+
+uint64_t spin_cycle = 0;
 
 uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
 
@@ -54,54 +60,65 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
     // define laser and fill with float
 	// minLaserDist[12] = {};
-    std::fill(minLaserDist, minLaserDist+nLasers, std::numeric_limits<float>::infinity());
+    std::fill(std::begin(minLaserDist), std::end(minLaserDist), std::numeric_limits<float>::infinity());
 
     // define average array
     // avgLaserDist[3] = {};
-    std::fill(avgLaserDist, avgLaserDist+nLasers, std::numeric_limits<float>::infinity());
+    std::fill(std::begin(avgLaserDist), std::end(avgLaserDist), std::numeric_limits<float>::infinity());
 
-    // nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
-    // desiredNLasers = desiredAngle*M_PI / (180*msg->angle_increment);
-	//ROS_INFO("Size of laser scan array: %i and size of offset: %i", nLasers, desiredNLasers);
+    nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
+    desiredNLasers = desiredAngle*M_PI / (180*msg->angle_increment);
+	ROS_INFO("Size of laser scan array: %i and size of offset: %i", nLasers, desiredNLasers);
 	
 	nLasers = 639;
 	desiredNLasers = 639;
-	uint32_t start_index = nLasers/2 - desiredNLasers;
-    uint32_t increment_size = 2*desiredNLasers/12;
+	// uint32_t start_index = nLasers/2 - desiredNLasers;
+    uint32_t increment_size = desiredNLasers/11;
 
-    for (uint32_t index_multiple = 0; index_multiple < 12; index_multiple++){
-        uint32_t local_start_index = start_index + increment_size*(index_multiple);
-        uint32_t local_end_index = start_index + increment_size*(index_multiple+1);
-
+    for (uint32_t index_multiple = 0; index_multiple < 11; ++index_multiple){
+        uint32_t local_start_index = increment_size*(index_multiple);
+        uint32_t local_end_index = increment_size*(index_multiple+1);
         for (uint32_t laser_idx = local_start_index; laser_idx < local_end_index; laser_idx++){
             minLaserDist[index_multiple] = std::min(minLaserDist[index_multiple], msg->ranges[laser_idx]);
         }
     }
-
-    int idx_start = 0;
-    // now take the average per each 3 segment
-    for (uint32_t i = 0; i < 3; i++){
-        idx_start = idx_start + i*3;
-        uint32_t idx_end = idx_start + (i+1)*3;
-        float local_avg = 0.0;
-
-        for (uint32_t j = idx_start; idx_end; j++){
-            // infinity filtering
-            // if (isinf(minLaserDist[j])){
-            //     minLaserDist[j] = 0.0;
-            // } 
+	ROS_INFO("Left: %f, Center: %f, Right: %f", minLaserDist[10],minLaserDist[5],minLaserDist[0]);
+    left_d = minLaserDist[10];
+	center_d = minLaserDist[5];
+	right_d = minLaserDist[0];
+	
+	uint16_t idx_start = 0;
+	uint16_t idx_end = 0;
+    //now take the average per each 3 segment
+   	for (uint16_t i = 0; i < 3; i++){
+		if (i==0){
+			idx_start = 0;
+			idx_end = 2;
+		}
+		else if (i == 1){
+			idx_start = 4;
+			idx_end = 6;
+		}
+		else if (i == 2){
+			idx_start = 8;
+			idx_end = 10;
+		}
+		float local_avg = 0.0;
+        for (uint32_t j = idx_start; j <= idx_end; j++){
             if (minLaserDist[j] == std::numeric_limits<float>::infinity()){ // infinity filtering new
                 minLaserDist[j] = 0.0;
             }
             local_avg += minLaserDist[j];
         }
 
-        avgLaserDist[i] = (float) local_avg / (float) 4;
+        avgLaserDist[i] = (float) local_avg / (float) 3;
     }
     ROS_INFO("AVG Left: %f, Center: %f Right: %f", avgLaserDist[2],avgLaserDist[1],avgLaserDist[0]);
-
-
+	avg_left = avgLaserDist[2];
+	avg_center = avgLaserDist[1];
+	avg_right = avgLaserDist[0];
 }
+
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr&msg)
 {
@@ -248,14 +265,14 @@ bool robot_move(const ROBOT_MOVEMENT move_type,ros::Publisher &vel_pub) {
 	else if (move_type == FORWARD) {
 		ROS_INFO("[ROBOT] FORWARD! \n");
 
-		vel.linear.x = 0.25;
+		vel.linear.x = reg_speed;
 		vel.angular.z = 0.0;
 	}
 
 	else if (move_type == BACKWARD) {
 		ROS_INFO("[ROBOT] BACKWARD! \n");
 
-		vel.linear.x = -0.25;
+		vel.linear.x = -reg_speed;
 		vel.angular.z = 0.0 ;
 	}
 
@@ -263,28 +280,28 @@ bool robot_move(const ROBOT_MOVEMENT move_type,ros::Publisher &vel_pub) {
 		ROS_INFO("[ROBOT] TURN LEFT! \n"); 
 
 		vel.linear.x = 0.0;
-		vel.angular.z = 0.1;
+		vel.angular.z = turn_speed;
 	}
 
 	else if (move_type == TURN_RIGHT) {
 		ROS_INFO("[ROBOT] TURN RIGHT! \n");
 
 		vel.linear.x = 0.0;
-		vel.angular.z = -0.1;
+		vel.angular.z = -turn_speed;
 	}
 
 	else if (move_type == GO_LEFT) {
 		ROS_INFO("[ROBOT] GO LEFT! \n");
 
-		vel.linear.x = 0.25;
-		vel.angular.z = 0.05;
+		vel.linear.x = reg_speed;
+		vel.angular.z = ang_speed;
 	}
 
 	else if (move_type == GO_RIGHT) {
 		ROS_INFO("[ROBOT] GO RIGHT! \n");
 
-		vel.linear.x = 0.25;
-		vel.angular.z = -0.05;
+		vel.linear.x = reg_speed;
+		vel.angular.z = -ang_speed;
 	}
 	else {
 		ROS_INFO("[ROBOT_MOVE] WRONG! \n");
@@ -296,8 +313,8 @@ bool robot_move(const ROBOT_MOVEMENT move_type,ros::Publisher &vel_pub) {
 	return true;
 }
 
-int state_Check() {
-	// 1 = find wall, 2 = 
+int state_Check() {// needs refinement
+	// 0 = find wall, 1 = align wall, 2 = Follow wall
 	if (center_d < f_limit && left_d > l_limit&& right_d < l_limit) {
 		return 1;
 	}
@@ -325,9 +342,6 @@ int state_Check() {
 	else if (center_d > f_limit&& left_d < l_limit && right_d < l_limit) {
 		return 2;
 	}
-
-	//center_d > f_limit && left_d > l_limit && right_d > l_limit
-
 	else {
 		return 0;
 	}
@@ -375,6 +389,19 @@ int wallFollow(bool wall_dir,ros::Publisher &vel_pub) {
 	return corner_counter;
 }
 
+void checkNear(){
+	if (avg_left < 0.65 || avg_center < 0.65 || avg_right < 0.65){
+		reg_speed = 0.1;
+		turn_speed = 0.8;
+		ang_speed = 0.25;
+	}
+	else{
+		reg_speed = 0.25;
+		turn_speed = 1;
+		ang_speed = 0.5;
+	}
+}
+
 
 int main(int argc, char **argv)
 {
@@ -399,26 +426,33 @@ int main(int argc, char **argv)
     
     while(ros::ok() && secondsElapsed <= 480) {
         ros::spinOnce();
+		checkNear();
 
         if (!checkBumperPressed(bumper,vel_pub)){
 			
 			int state = state_Check();
+			ROS_INFO("Robot State: %i\n", state);
+
+			// if (spin_cycle >= spin_30){
+			// 	rotate(0.5, 360, vel_pub);
+			// 	spint_cycle
+			// }
 
 			//find a wall
 			if (state == 0) {
-				ROS_INFO("find a wall\n");
+				ROS_INFO("Find a wall");
 				robot_move(FORWARD,vel_pub);
 			}
 
 			//align
 			else if (state == 1) {
-				ROS_INFO("align \n");
+				ROS_INFO("Align wall");
 				robot_move(TURN_LEFT,vel_pub);
 			}
 
 			//follow wall
-			else {
-				ROS_INFO("follow wall\n");
+			else if (state  == 2){
+				ROS_INFO("Follow wall");
 				std::chrono::time_point<std::chrono::system_clock> start_cycle;
 				start_cycle = std::chrono::system_clock::now();
 				uint64_t cycle_time_limit = random() % 10 + 25;
@@ -427,35 +461,8 @@ int main(int argc, char **argv)
 				int turn_dir;
 
 				turn_dir = rand() % 2;
-					
-				if (turn_dir) {
-					ROS_INFO("rotate 180\n");
-					rotate(0.2, 180.0, vel_pub);
-					ros::spinOnce();
-				}
-				
 				corner_counter = wallFollow(turn_dir,vel_pub);
 				
-				// while (cycle_time <= cycle_time_limit || corner_counter < 3) {
-				// 	cycle_time = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start_cycle).count();
-
-
-				// 	corner_counter = wallFollow(turn_dir,vel_pub);
-				// 	ros::spinOnce();
-				// 	checkBumperPressed(bumper,vel_pub);
-				// 	if (cycle_time > 15 && corner_counter < 1)
-				// 		break;
-				// }
-
-				double deg = rand() % 61 + 60;
-				if (turn_dir) {
-					ROS_INFO("random turn right\n");
-					rotate(0.2, -deg, vel_pub);
-				}
-				else {
-					ROS_INFO("random turn left\n");
-					rotate(0.2, deg, vel_pub);
-				}
 			}
         }
 
